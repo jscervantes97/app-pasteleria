3aee52ebb90bbf347d2996391b4dc1cce144e326
Jesus Alberto Cervantes Medina <jscervantesmedina@gmail.com>
2024-05-05 16:09:52 -0700

    se agrego configuracion allowhosts

--- pasteleriaapp/settings.py
+++ pasteleriaapp/settings.py
@@ -128,6 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = [
+    'localhost',
+    'app-pasteleria-production.up.railway.app'
+]
 # En settings.py
 
 #import locale
