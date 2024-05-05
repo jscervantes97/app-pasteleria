b6041722c75321ead128e150aeb83408994d7927
Jesus Alberto Cervantes Medina <jscervantesmedina@gmail.com>
2024-05-05 15:31:13 -0700

    se cambio zona para el idioma

--- pasteleriaapp/settings.py
+++ pasteleriaapp/settings.py
@@ -109,7 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'es-mx'
+#LANGUAGE_CODE = 'es-mx'
+LANGUAGE_CODE = 'en-us'
+
 
 TIME_ZONE = 'UTC'
 
