1d8a8a1e31d4a52708338830b5c3f3892ad7cfd1
Jesus Alberto Cervantes Medina <jscervantesmedina@gmail.com>
2024-05-20 01:31:48 -0700

    pequeño fix porque se me fue cambiar la url de bd equis de

--- pasteleriaapp/settings.py
+++ pasteleriaapp/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'pasteleriaapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-"""DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
-}"""
-
 DATABASES = {
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
+
+"""DATABASES = {
    'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pasteleriadb',
@@ -100,7 +100,7 @@ DATABASES = {
         'HOST' : 'localhost' ,
         'PORT' : '5432'
     }
-}
+}"""
 
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
