fcf6a86eabb36ea223ca09be44f51caea15e448c
Jesus Cervantes <jscervantesmedina@gmail.com>
2024-11-06 22:47:41 -0700

    cambio de zona horaria a la aplicacion

--- pasteleriaapp/settings.py
+++ pasteleriaapp/settings.py
@@ -135,11 +135,14 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'es-mx'
 
 
-TIME_ZONE = 'UTC'
-
+#TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Mexico_City'
 USE_I18N = True
 
 USE_TZ = True
+
+
+#USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
