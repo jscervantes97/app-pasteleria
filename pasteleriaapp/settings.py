f50552792e13a5a78cf4819e31a5083348e25c32
Jesus Cervantes <jscervantesmedina@gmail.com>
2024-11-07 20:16:32 -0700

    fix en settings

--- pasteleriaapp/settings.py
+++ pasteleriaapp/settings.py
@@ -55,13 +55,13 @@ DATEPICKER_INCLUDE_JQUERY = True
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    'corsheaders.middleware.CorsMiddleware'
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'pasteleriaapp.urls'
